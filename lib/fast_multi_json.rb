00e7515f1c7b4615031d0abedb8714f543be38a6
Benjamin Fleischer <github@benjaminfleischer.com>
2018-02-18 18:41:22 -0600

    Refactor FastMultiJSON and add from_json

--- lib/fast_multi_json.rb
+++ lib/fast_multi_json.rb
@@ -1,45 +1,43 @@
-require "fast_multi_json/version"
+# frozen_string_literal: true
+
+require 'fast_multi_json/version'
 require 'logger'
 
 # Usage:
 #   class Movie
 #     def to_json(payload)
-#       FastMultiJson.to_json(payload)
+#       ::FastMultiJson.to_json(payload)
+#     end
+#
+#     def from_json(string)
+#       ::FastMultiJson.from_json(string)
 #     end
 #   end
 module FastMultiJson
-  # Result object pattern is from https://johnnunemaker.com/resilience-in-ruby/
-  # e.g. https://github.com/github/github-ds/blob/fbda5389711edfb4c10b6c6bad19311dfcb1bac1/lib/github/result.rb
-  class Result
-    def initialize
-      @value = yield
-      @error = nil
-    rescue LoadError => e
-      @error = e
-    end
-
-    def ok?
-      @error.nil?
-    end
-
-    def value!
-      if ok?
-        @value
-      else
-        raise @error
-      end
-    end
-
-    def rescue
-      return self if ok?
-      Result.new { yield(@error) }
-    end
-  end
-  private_constant :Result
-
   def self.logger(logger=nil)
     return @logger = logger if logger
     @logger ||= Logger.new(IO::NULL)
+  end
+
+  def self.to_json(object)
+    _fast_to_json(object)
+  rescue NameError
+    define_to_json(::FastMultiJson)
+    _fast_to_json(object)
+  end
+
+  def self.from_json(string)
+    _fast_from_json(string)
+  rescue NameError
+    define_from_json(::FastMultiJson)
+    _fast_from_json(string)
+  end
+
+  def self.define_to_json(receiver)
+    cl = caller_locations[0]
+    method_body = to_json_method
+    logger.debug { "Defining #{receiver}._fast_to_json as #{method_body.inspect}" }
+    receiver.instance_eval method_body, cl.absolute_path, cl.lineno
   end
 
   # Encoder-compatible with default MultiJSON adapters and defaults
@@ -70,22 +68,62 @@ module FastMultiJson
     encode_method << "\nend"
   end
 
-  def self.to_json(object)
-    _fast_to_json(object)
-  rescue NameError
-    define_to_json(FastMultiJson)
-    _fast_to_json(object)
-  end
-
-  def self.define_to_json(receiver)
-    cl = caller_locations[0]
-    method_body = to_json_method
-    logger.debug { "Defining #{receiver}._fast_to_json as #{method_body.inspect}" }
-    receiver.instance_eval method_body, cl.absolute_path, cl.lineno
-  end
-
   def self.reset_to_json!
     undef :_fast_to_json if method_defined?(:_fast_to_json)
     logger.debug { "Undefining #{receiver}._fast_to_json" }
   end
+
+  def self.define_from_json(receiver)
+    cl = caller_locations[0]
+    method_body = from_json_method
+    logger.debug { "Defining #{receiver}._fast_from_json as #{method_body.inspect}" }
+    receiver.instance_eval method_body, cl.absolute_path, cl.lineno
+  end
+
+  # Decoder-compatible with default MultiJSON adapters and defaults
+  def self.from_json_method
+    decode_method = String.new(%(def _fast_from_json(string)\n ))
+    decode_method << Result.new {
+      require 'oj'
+      %(::Oj.load(string))
+    }.rescue {
+      require 'json'
+      %(JSON.parse(string))
+    }.value!
+    decode_method << "\nend"
+  end
+
+  def self.reset_from_json!
+    undef :_fast_from_json if method_defined?(:_fast_from_json)
+    logger.debug { "Undefining #{receiver}._fast_from_json" }
+  end
+
+  # Result object pattern is from https://johnnunemaker.com/resilience-in-ruby/
+  # e.g. https://github.com/github/github-ds/blob/fbda5389711edfb4c10b6c6bad19311dfcb1bac1/lib/github/result.rb
+  class Result
+    def initialize
+      @value = yield
+      @error = nil
+    rescue LoadError => e
+      @error = e
+    end
+
+    def ok?
+      @error.nil?
+    end
+
+    def value!
+      if ok?
+        @value
+      else
+        raise @error
+      end
+    end
+
+    def rescue
+      return self if ok?
+      Result.new { yield(@error) }
+    end
+  end
+  private_constant :Result
 end
